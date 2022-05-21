d97ddf6f8dfc32c7e1f4ee320011db94cc946598
Mahin Hossen <mahinhossen001@gmail.com>
2022-05-21 21:02:48 +0600

    Time: 829 ms (51.75%), Space: 12.8 MB (83.88%) - LeetHub

---
+++ 212-word-search-ii/212-word-search-ii.cpp
@@ -0,0 +1,88 @@
+class Solution {
+public:
+    
+    vector<int>dr{-1,0,1,0,-1};
+
+    class TrieNode
+    {
+    public:
+        string word;
+        TrieNode* next[26];
+
+        TrieNode()
+        {
+            word = "";
+            for(int i=0;i<26;i++)
+                next[i]=NULL;
+        }
+    };
+
+
+    TrieNode* generateTrie(vector<string> &words)
+    {
+        TrieNode* root = new TrieNode();
+
+        for(auto &wd : words)
+        {
+            TrieNode* currNode = root;
+            for(auto &c : wd)
+            {
+                if(currNode->next[c-'a']==NULL)
+                {
+                    currNode->next[c-'a'] = new TrieNode();
+                } 
+                currNode = currNode->next[c-'a'];
+            }
+            currNode->word = wd;
+        }
+        return root;
+    }
+
+    void dfs(int x,int y,TrieNode* currNode,vector<vector<char>>&board,vector<string> &wordsOnBoard)
+    {
+        char ch = board[x][y];
+        if(currNode==NULL || currNode->next[ch-'a']==NULL) return;    
+        currNode = currNode->next[ch-'a']; 
+
+        if(currNode->word.size()>0)
+        {
+            wordsOnBoard.push_back(currNode->word); 
+            currNode->word.clear();
+        }    
+
+        board[x][y]='#';
+
+        for(int i=0;i<4;i++)
+        {
+            int X = x+dr[i];
+            int Y = y+dr[i+1];
+
+            if(X<0 || Y<0 || X>=board.size() || Y>=board[0].size() || board[X][Y]=='#') 
+                continue;
+
+            dfs(X,Y,currNode,board,wordsOnBoard);
+
+        }
+        board[x][y]=ch;
+    }
+
+    vector<string> findWords(vector<vector<char>>&board, vector<string>&words)
+    {
+        TrieNode* root = generateTrie(words);
+
+        vector<string> wordsOnBoard;
+        int n = board.size();
+        int m = board[0].size();
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                dfs(i,j,root,board,wordsOnBoard);
+            }
+        }
+
+        return wordsOnBoard;
+    }
+
+};
