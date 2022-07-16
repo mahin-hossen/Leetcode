3345d04f9feb747ae77fd8bfa7a4a0cc56b724db
Mahin Hossen <mahinhossen001@gmail.com>
2022-07-16 13:51:54 +0600

    Time: 63 ms (36.64%), Space: 12.3 MB (83.64%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    const int dirs[5] = {-1,0,1,0,-1};
+    int n, m;
+    void traverseIslands(int x,int y,vector<vector<char>>& grid)
+    {
+        grid[x][y] = '0';
+        for(int i=0;i<4;i++)
+        {
+            int X = x+dirs[i];
+            int Y = y+dirs[i+1];
+            if(X>=0 && Y>=0 && X<n && Y<m && grid[X][Y]=='1')//unvisited
+            {
+                traverseIslands(X,Y,grid);                
+            }
+        }
+    }
+    int numIslands(vector<vector<char>>& grid) 
+    {
+        n = grid.size();
+        m = grid[0].size();
+        int island_count = 0;
+        
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]=='1')//unvisited
+                {
+                    traverseIslands(i,j,grid);
+                    island_count++;
+                }
+                    
+            }
+        }
+        
+        return island_count;
+    }
+};
