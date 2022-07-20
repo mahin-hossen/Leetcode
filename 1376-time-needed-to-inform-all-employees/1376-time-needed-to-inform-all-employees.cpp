504af66c41bbba9bf0d903cd729359cc150d476a
Mahin Hossen <mahinhossen001@gmail.com>
2022-07-20 13:36:17 +0600

    Time: 185 ms (98.85%), Space: 90.7 MB (96.22%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-vector<vector<int>>adjacentNodes;
-int totalMinutes(int headID,vector<int>& informTime)
+int sumOfMinutes(int headID, vector<int>& manager, vector<int>& informTime) 
+{
+    if(manager[headID]!=-1)
+    {
+        informTime[headID] += sumOfMinutes(manager[headID],manager,informTime);
+        manager[headID] = -1;    
+    }   
+    return informTime[headID];
+    
+}
+    
+int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) 
 {
     int minutes = 0;
-    for(auto subordinate : adjacentNodes[headID])
-    {        
-        minutes = max(minutes,totalMinutes(subordinate,informTime));
-    }
-    return minutes+informTime[headID];
-}
-
-int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) 
-{    
-    adjacentNodes.clear();
-    adjacentNodes.resize(n+1);
     for(int i=0;i<n;i++)
     {
-        if(manager[i]!=-1)
-            adjacentNodes[manager[i]].push_back(i);
+        minutes = max(minutes,sumOfMinutes(i,manager,informTime));
     }
-
-    return totalMinutes(headID,informTime);
+    return minutes;
 }
 };
