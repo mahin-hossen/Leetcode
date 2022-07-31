29fd30e0f3a66684f8004171385a5438d21ee6e3
Mahin Hossen <mahinhossen001@gmail.com>
2022-07-31 14:44:14 +0600

    Time: 79 ms (29.33%), Space: 22.8 MB (16.25%) - LeetHub

--- 255-verify-preorder-sequence-in-binary-search-tree/255-verify-preorder-sequence-in-binary-search-tree.cpp
+++ 255-verify-preorder-sequence-in-binary-search-tree/255-verify-preorder-sequence-in-binary-search-tree.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    
-void traversePreorder(vector<int>& preorder,int &currIdx,int L,int R)
+
+bool traversePreorder(vector<int>& preorder,int &currIdx,int L,int R)
 {    
-    if(currIdx==preorder.size()) return;
+    if(currIdx==preorder.size()) return true;
     
     int V = preorder[currIdx];
-    if(V<L || V>R) return;
+    if(V<L || V>R) return false;
     currIdx++;
     
-    traversePreorder(preorder,currIdx,L,V-1);
-    traversePreorder(preorder,currIdx,V+1,R);
+    auto left = traversePreorder(preorder,currIdx,L,V-1);
+    auto right = traversePreorder(preorder,currIdx,V+1,R);
+    return left||right;
 }
 bool verifyPreorder(vector<int>& preorder)
 {
     int currIdx = 0;
-    traversePreorder(preorder,currIdx,INT_MIN,INT_MAX);
-    return currIdx==preorder.size();
+    return traversePreorder(preorder,currIdx,INT_MIN,INT_MAX);
 }
 
 };
