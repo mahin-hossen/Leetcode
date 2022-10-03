4ec4d7904e53a49b21c0198a578218e9e505fec6
Mahin Hossen <mahinhossen001@gmail.com>
2022-10-03 21:32:37 +0600

    Time: 11 ms (55.36%), Space: 10 MB (80.13%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-    /*
-    6 6 7
-    6 6 7
-    */
+    //TC : nlogn
+    //MC : 0(1)
+    //using binary search & if already sorted
     vector<int>setIntersect(vector<int>&nums1,vector<int>& nums2)
     {
-        vector<int> intersect;
-        int num1Sz = nums1.size(), num2Sz = nums2.size();
-        for(int i=0,j=0;i<num1Sz && j<num2Sz;)
+        vector<int>intersect;
+        int sz = nums1.size();     
+        
+        for(int i=0;i<sz;i++)
         {
-            while(i+1<num1Sz && nums1[i]==nums1[i+1]) i++;
-            while(j+1<num2Sz && nums2[j]==nums2[j+1]) j++;
-            
-            if(nums1[i]>nums2[j])  j++;
-            else if(nums1[i]==nums2[j])
+            while(i+1<sz && nums1[i]==nums1[i+1]) i++;//for duplicate check
+            if(binary_search(nums2.begin(),nums2.end(),nums1[i]))
             {
-                intersect.push_back(nums1[i]);               
-                i++,j++;
+                intersect.push_back(nums1[i]);
             }
-            else i++;
         }
+        
         return intersect;
     }
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) 
@@ -30,7 +26,7 @@ public:
         sort(nums2.begin(),nums2.end());
         vector<int>intersect;
         
-        intersect = nums1.size()>nums2.size() ? setIntersect(nums1,nums2) : setIntersect(nums2,nums1);        
+        intersect = nums1.size()<nums2.size() ? setIntersect(nums1,nums2) : setIntersect(nums2,nums1);        
         
         return intersect;
     }
