e79e966bb8358d8a221caa2237bfbfd5e952a017
Mahin Hossen <mahinhossen001@gmail.com>
2022-10-09 21:05:55 +0600

    Time: 3 ms (41.53%), Space: 6.6 MB (30.79%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,5 +1,19 @@
 class Solution {
 public:
+    
+    vector<int> makeTriangle(vector<vector<int>>& triangle,vector<int>& lastRow)
+    {
+        vector<int> newRow{1};
+        for(int i=1;i<lastRow.size();i++)
+        {
+            newRow.push_back(lastRow[i-1]+lastRow[i]);
+        }
+        newRow.push_back(1);
+        
+        triangle.push_back(newRow);        
+        return newRow;
+    }
+    
     vector<vector<int>> generate(int numRows) 
     {
         /*
@@ -8,28 +22,15 @@ public:
       1 2 1
      1 3 3 1
     1 4 6 4 1
-        */
-        /*
-        TC: 0(n!)
-        MC: 0(n!) + 0(n) = n!
-        */
-        vector<vector<int>> triangle {{1}};
-        vector<int>lastRow = {1};
-        vector<int>newRow;
+        */        
+        vector<vector<int>>triangle{{1}};
+        vector<int>lastRow{1};
         
-        for(int row=1;row<numRows;row++)//n*2 3 4//n=5
+        for(int row=1;row<numRows;row++)
         {
-            newRow.push_back(1);
-            for(int i=1;i<lastRow.size();i++)//
-            {
-                newRow.push_back(lastRow[i]+lastRow[i-1]);
-            }
-            newRow.push_back(1);
-            triangle.push_back(newRow);
-            lastRow = newRow; //0(n)
-            newRow.clear();
+            lastRow = makeTriangle(triangle,lastRow);
         }
         
-        return triangle;        
+        return triangle;       
     }
 };
