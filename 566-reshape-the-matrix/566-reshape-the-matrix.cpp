eb582f6d6a4f1d3805dc9062953fe3dcf49f7800
Mahin Hossen <mahinhossen001@gmail.com>
2022-10-09 08:23:41 +0600

    Time: 7 ms (98.23%), Space: 10.8 MB (59.44%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.cpp
+++ 566-reshape-the-matrix/566-reshape-the-matrix.cpp
@@ -6,24 +6,20 @@ public:
         int n = mat.size();
         if(m*n!=r*c) return mat;
 
-        vector<vector<int>> reshapedMatrix;
+        vector<vector<int>> reshapedMatrix(r,vector<int>(c));
         vector<int> mat2;
-        int col = 0;
+        int col = 0, row = 0;
 
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                mat2.push_back(mat[i][j]);
-                col++;
-                if(col==c)
-                {
-                    reshapedMatrix.push_back(mat2);
-                    mat2.clear();
-                    col=0;
-                 }
+			  reshapedMatrix[row][col] = mat[i][j];
+			  col++;
+			  if(col==c) col=0,row++;
             }
         }
+
         
         return reshapedMatrix;
     }
