a0b6785127c683b8622cf144dc393664a255c77f
Mahin Hossen <mahinhossen001@gmail.com>
2022-10-20 19:07:48 +0600

    Time: 10 ms (76.77%), Space: 14.8 MB (81.61%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -22,12 +22,13 @@ public:
         {            
             if(list1->val < list2->val)
             {
-                mergedList->next = new ListNode(list1->val);
+                // mergedList->next = new ListNode(list1->val);
+                mergedList->next = list1;
                 list1 = list1->next;
             }
             else
             {
-                mergedList->next = new ListNode(list2->val);
+                mergedList->next = list2;
                 list2 = list2->next;
             }
             mergedList = mergedList->next;
@@ -35,13 +36,13 @@ public:
         
         while(list1)
         {
-            mergedList->next = new ListNode(list1->val);
+            mergedList->next = list1;
             mergedList = mergedList->next;
             list1 = list1->next;
         }
         while(list2)
         {
-            mergedList->next = new ListNode(list2->val);
+            mergedList->next = list2;
             mergedList = mergedList->next;
             list2 = list2->next;
         }
