f9ae340ecbe970d186031999dc50866c45b52eff
Mahin Hossen <mahinhossen001@gmail.com>
2022-10-31 22:37:50 +0600

    Time: 19 ms (7.48%), Space: 16.3 MB (93.52%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -11,33 +11,17 @@
  */
 class Solution {
 public:
+    bool isSymmetricHelper(TreeNode* left, TreeNode* right)
+    {
+        
+        if(!left && !right) return true;
+        if(!left || !right) return false;
+        if(left->val!=right->val) return false;
+        
+        return isSymmetricHelper(left->left,right->right) && isSymmetricHelper(left->right,right->left);
+    }
     bool isSymmetric(TreeNode* root) 
-    {        
-        queue<TreeNode*> leftBucket, rightBucket;
-     
-        leftBucket.push(root);
-        rightBucket.push(root);
-        
-        while(!leftBucket.empty())
-        {
-            auto leftNode = leftBucket.front();
-            auto rightNode = rightBucket.front();
-            leftBucket.pop();
-            rightBucket.pop();
-            
-            if(!leftNode && !rightNode) continue;
-            if(!leftNode || !rightNode) return false;
-            if(leftNode->val!=rightNode->val) return false;
-            
-            leftBucket.push(leftNode->left);
-            leftBucket.push(leftNode->right);
-            
-            rightBucket.push(rightNode->right);
-            rightBucket.push(rightNode->left);
-        }
-        
-        if(leftBucket.size()!=0 || rightBucket.size()!=0) return false;// one of the side havent fully traversed
-        return true;
-        
+    {
+        return isSymmetricHelper(root->left,root->right);
     }
 };
