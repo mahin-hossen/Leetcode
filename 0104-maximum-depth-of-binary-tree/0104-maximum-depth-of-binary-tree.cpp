c9b4c2d3a44f8d3774e82cb1bb9518e3d23941ea
Mahin Hossen <mahinhossen001@gmail.com>
2022-10-31 21:29:44 +0600

    Time: 15 ms (64.47%), Space: 18.8 MB (89.12%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -17,23 +17,10 @@ public:
         //TC 0(N)
         if(!root) return 0;
         
-        queue<pair<TreeNode*,int>> bucket;
-        int depthCount;
-        bucket.push({root,1});        
+        auto left = maxDepth(root->left);
+        auto right = maxDepth(root->right);
         
-        while(!bucket.empty())
-        {
-            auto top = bucket.front();
-            bucket.pop();
-            
-            auto node = top.first;
-            auto depth = top.second;
-            depthCount = max(depthCount,depth);
-            
-            if(node->left) bucket.push({node->left,depth+1});
-            if(node->right) bucket.push({node->right,depth+1});
-        }
-        return depthCount;
+        return max(left+1,right+1);
         
     }
 };
