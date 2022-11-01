6558a7801332320d34ab4a612032ecfaa828d79c
Mahin Hossen <mahinhossen001@gmail.com>
2022-11-01 20:58:12 +0600

    Time: 0 ms (100.00%), Space: 9.7 MB (37.45%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -13,33 +13,20 @@ class Solution {
 public:
     void invertTreeHelper(TreeNode* &leftNode, TreeNode* &rightNode)
     {
-        if(!leftNode && !rightNode) 
-        {
-            return;
-        }
+        if(!leftNode && !rightNode)  return;
         if(leftNode && !rightNode)
         {
             swap(leftNode,rightNode);
-            // auto dummy = leftNode;
-            // leftNode = rightNode;
-            // rightNode = new TreeNode();
-            // rightNode = dummy;
             return invertTreeHelper(rightNode->left,rightNode->right);
         }
         if(!leftNode && rightNode)
         {
-            // auto dummy = rightNode;
-            // rightNode = leftNode;
-            // // leftNode = new TreeNode();
-            // leftNode = dummy;
             swap(leftNode,rightNode);
             return invertTreeHelper(leftNode->left,leftNode->right);
 
         }   
 
-        int dummy = leftNode->val;
-        leftNode->val = rightNode->val;
-        rightNode->val = dummy;
+        swap(leftNode->val,rightNode->val);
 
         invertTreeHelper(leftNode->left,rightNode->right);
         invertTreeHelper(leftNode->right,rightNode->left);
